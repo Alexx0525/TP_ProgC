4636c7cd43af7e607610de9ee0282f522fa4414b
LAUDRIN Alex <alex.laudrin25@gmail.com>
2025-04-04 11:10:25 +0200

    7.1

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -4,15 +4,24 @@ int main() {
     int somme = 0;
 
     for (int i = 1; i <= 1000; i++) {
-        if (i % 11 == 0)
-            continue;
+        if (i % 11 == 0) {
+            continue; // ne pas ajouter si divisible par 11
+        }
 
-        if (i % 5 == 0 || i % 7 == 0)
+        if (i % 5 == 0 || i % 7 == 0) {
             somme += i;
 
-        if (somme > 5000)
-            break;
+    if (somme > 5000) {
+            somme -= i;
+            break; // arrêter si la somme dépasse 5000
+    }
+            
+        }
     }
 
+   
+
     printf("Somme finale : %d\n", somme);
+
+    return 0;
 }
